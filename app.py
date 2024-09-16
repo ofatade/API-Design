2dd44798a35e917b9927f5fed2e5d2c5811b2804
ofatade <dfatade@gmail.com>
2024-09-15 21:53:17 -0400

    Documentation

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from database import db
 from models.schemas import ma
 from limiter import limiter
 from cache import cache
+from flask_swagger_ui import get_swaggerui_blueprint
 
 
 #models
@@ -17,6 +18,12 @@ from models.customerCart import customer_cart
 from routes.customerBP import customer_blueprint
 from routes.productBP import product_blueprint
 from routes.orderBP import order_blueprint
+
+#SWAGGER
+SWAGGER_URL = '/api/docs' #url endpoint to view our docs
+API_URL = '/static/swagger.yaml' #grabs our host from our swagger.yaml
+
+swagger_blueprint = get_swaggerui_blueprint(SWAGGER_URL, API_URL, config={'app_name': 'Ecommerce API'})
 
 def create_app(config_name):
 
@@ -35,10 +42,12 @@ def blueprint_config(app):
     app.register_blueprint(customer_blueprint, url_prefix="/customers")
     app.register_blueprint(product_blueprint, url_prefix="/products")
     app.register_blueprint(order_blueprint, url_prefix="/orders")
+    app.register_blueprint(swagger_blueprint, url_prefix=SWAGGER_URL)
 
 def rate_limit_config(app):
     limiter.init_app(app)
     limiter.limit("100 per hour")(customer_blueprint)
+    limiter.limit("10 per second")(swagger_blueprint)
 
 if __name__ == '__main__':
     app = create_app('DevelopmentConfig')
